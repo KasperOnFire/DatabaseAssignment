4abc1794833b32281107790525cab0c7e8b47c12
Kasper Ravn Breindal <breindal70@gmail.com>
2016-11-17 13:32:01 +0100

    removed sql stuff form dao

--- src/dbassignment/DataAccessObject.java
+++ src/dbassignment/DataAccessObject.java
@@ -1,11 +1,6 @@
 package dbassignment;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public interface DataAccessObject {
 
@@ -26,19 +21,4 @@ public interface DataAccessObject {
     public User getUser(String username);
 
     // Access SQL
-    public void test() {
-        try {
-            DBConnector db = new DBConnector();
-            Connection conn = db.getConnection();
-            Statement stmt = conn.createStatement();
-            String sql = "select * from dept where loc = 'CHICAGO'";
-            ResultSet rs = stmt.executeQuery(sql);
-            if (rs.next()) {
-                System.out.println("Dept no: " + rs.getInt("deptno"));
-            }
-
-        } catch (Exception ex) {
-            Logger.getLogger(DataAccessObject.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
 }
